99359f497555be563535f49b825642a821c13696
Alicja Ruszkowska <alicjaaruszkowska@gmail.com>
2023-06-22 22:21:55 +0200

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool isFemale = true;
+var age = 33;
+
+if (isFemale)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta powyżej 33 lat");
+    }
+    else if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+
+}
+else
+{
+    if (age > 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
